5b87b08af0ee8c172eabc814f4f9b193728cfc85
Pieter De Gendt <pieter.degendt@basalte.be>
2017-08-30 14:51:38 +0200

    Fix "sphinx.util.compat.Directive is deprecated" warning

--- src/redjack/sphinx/lua.py
+++ src/redjack/sphinx/lua.py
@@ -10,7 +10,7 @@
 import re
 
 from docutils import nodes
-from docutils.parsers.rst import directives
+from docutils.parsers.rst import Directive, directives
 
 from sphinx import addnodes
 from sphinx.roles import XRefRole
@@ -18,7 +18,6 @@ from sphinx.locale import l_, _
 from sphinx.domains import Domain, ObjType, Index
 from sphinx.directives import ObjectDescription
 from sphinx.util.nodes import make_refnode
-from sphinx.util.compat import Directive
 from sphinx.util.docfields import Field, GroupedField, TypedField
 
 
